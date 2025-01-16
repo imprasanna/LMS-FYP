85f6be412c5030da32b1d9e264759727da50de2e
Prasanna Acharya <prasannaacharya2073@gmail.com>
2025-01-16 11:22:37 +0545

    Made changes for state management and rendering of subjects

--- frontend/src/redux/sclassRelated/sclassHandle.js
+++ frontend/src/redux/sclassRelated/sclassHandle.js
@@ -1,4 +1,6 @@
 import axios from "axios";
+import { createSlice } from "@reduxjs/toolkit";
+
 import {
   getRequest,
   getSuccess,
@@ -74,7 +76,7 @@ export const getSubjectList = (id, address) => async (dispatch) => {
       dispatch(getSubjectsSuccess(result.data));
     }
   } catch (error) {
-    dispatch(getError(error));
+    dispatch(getError(error.message));
   }
 };
 
@@ -127,3 +129,80 @@ export const deleteAllClasses = (adminID) => async (dispatch) => {
     throw error;
   }
 };
+
+const initialState = {
+  sclassesList: [],
+  sclassStudents: [],
+  sclassDetails: [],
+  subjectsList: [],
+  subjectDetails: [],
+  loading: false,
+  subloading: false,
+  error: null,
+  response: null,
+  getresponse: null,
+};
+
+const sclassSlice = createSlice({
+  name: "sclass",
+  initialState,
+  reducers: {
+    getRequest: (state) => {
+      state.loading = true;
+    },
+    getSubDetailsRequest: (state) => {
+      state.subloading = true;
+    },
+    getSuccess: (state, action) => {
+      state.sclassesList = action.payload;
+      state.loading = false;
+      state.error = null;
+      state.getresponse = null;
+    },
+    getStudentsSuccess: (state, action) => {
+      state.sclassStudents = action.payload;
+      state.loading = false;
+      state.error = null;
+      state.getresponse = null;
+    },
+    getSubjectsSuccess: (state, action) => {
+      state.subjectsList = action.payload;
+      state.loading = false;
+      state.error = null;
+      state.response = null;
+    },
+    getFailed: (state, action) => {
+      state.subjectsList = [];
+      state.response = action.payload;
+      state.loading = false;
+      state.error = null;
+    },
+    getFailedTwo: (state, action) => {
+      state.sclassesList = [];
+      state.sclassStudents = [];
+      state.getresponse = action.payload;
+      state.loading = false;
+      state.error = null;
+    },
+    getError: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+    detailsSuccess: (state, action) => {
+      state.sclassDetails = action.payload;
+      state.loading = false;
+      state.error = null;
+    },
+    getSubDetailsSuccess: (state, action) => {
+      state.subjectDetails = action.payload;
+      state.subloading = false;
+      state.error = null;
+    },
+    resetSubjects: (state) => {
+      state.subjectsList = [];
+      state.sclassesList = [];
+    },
+  },
+});
+
+export const sclassReducer = sclassSlice.reducer;
