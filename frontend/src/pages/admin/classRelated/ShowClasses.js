475f19493db3b5f446a42a68ba91481c101dd98a
Prasanna Acharya <prasannaacharya2073@gmail.com>
2025-01-20 11:26:37 +0545

    Made changes with UI and pagination functioanlity for classes page of Admin dashboard

--- frontend/src/pages/admin/classRelated/ShowClasses.js
+++ frontend/src/pages/admin/classRelated/ShowClasses.js
@@ -6,6 +6,10 @@ import {
   MenuItem,
   ListItemIcon,
   Tooltip,
+  Paper,
+  Select,
+  MenuItem as MuiMenuItem,
+  Button,
 } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
 import { useDispatch, useSelector } from "react-redux";
@@ -15,11 +19,6 @@ import {
   getAllSclasses,
 } from "../../../redux/sclassRelated/sclassHandle";
 import { BlueButton, GreenButton } from "../../../components/buttonStyles";
-import TableTemplate from "../../../components/TableTemplate";
-
-import SpeedDialIcon from "@mui/material/SpeedDialIcon";
-import PostAddIcon from "@mui/icons-material/PostAdd";
-import PersonAddAlt1Icon from "@mui/icons-material/PersonAddAlt1";
 import styled from "styled-components";
 import Popup from "../../../components/Popup";
 import ConfirmationDialog from "../../../components/ConfirmationDialog";
@@ -28,23 +27,28 @@ const ShowClasses = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
-  const { sclassesList, loading, error, getresponse } = useSelector(
-    (state) => state.sclass
-  );
+  const { sclassesList, loading, error } = useSelector((state) => state.sclass);
   const { currentUser } = useSelector((state) => state.user);
 
   const adminID = currentUser._id;
+
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+  const [filteredClasses, setFilteredClasses] = useState([]);
+  const [showPopup, setShowPopup] = useState(false);
+  const [message, setMessage] = useState("");
+  const [confirmDialogOpen, setConfirmDialogOpen] = useState(false);
+  const [deleteID, setDeleteID] = useState(null);
 
   useEffect(() => {
     dispatch(getAllSclasses(adminID, "Sclass"));
   }, [adminID, dispatch]);
 
-  if (error) {
-    console.log(error);
-  }
-
-  const [showPopup, setShowPopup] = useState(false);
-  const [message, setMessage] = useState("");
+  useEffect(() => {
+    setFilteredClasses(
+      sclassesList.slice(page * rowsPerPage, (page + 1) * rowsPerPage)
+    );
+  }, [sclassesList, page, rowsPerPage]);
 
   const deleteHandler = (deleteID) => {
     dispatch(deleteClass(deleteID))
@@ -59,9 +63,6 @@ const ShowClasses = () => {
         console.error(err);
       });
   };
-
-  const [confirmDialogOpen, setConfirmDialogOpen] = useState(false);
-  const [deleteID, setDeleteID] = useState(null);
 
   const openConfirmDialog = (id) => {
     setDeleteID(id);
@@ -80,154 +81,107 @@ const ShowClasses = () => {
     closeConfirmDialog();
   };
 
-  const sclassColumns = [{ id: "name", label: "Class Name", minWidth: 170 }];
+  const handleRowsPerPageChange = (event) => {
+    setRowsPerPage(Number(event.target.value));
+    setPage(0);
+  };
 
-  const sclassRows = sclassesList?.map((sclass) => ({
-    name: sclass.sclassName,
-    id: sclass._id,
-  }));
+  const handlePageChange = (direction) => {
+    if (
+      direction === "next" &&
+      page < Math.ceil(sclassesList.length / rowsPerPage) - 1
+    ) {
+      setPage(page + 1);
+    } else if (direction === "prev" && page > 0) {
+      setPage(page - 1);
+    }
+  };
 
-  const SclassButtonHaver = ({ row }) => {
-    const actions = [
-      {
-        icon: <PostAddIcon />,
-        name: "Add Subjects",
-        action: () => navigate("/Admin/addsubject/" + row.id),
-      },
-      {
-        icon: <PersonAddAlt1Icon />,
-        name: "Add Student",
-        action: () => navigate("/Admin/class/addstudents/" + row.id),
-      },
-    ];
-    return (
+  const SclassRow = ({ sclass }) => (
+    <HoverableDiv
+      onClick={() => navigate("/Admin/classes/class/" + sclass._id)}
+    >
+      <span>{sclass.sclassName}</span>
       <ButtonContainer>
-        <IconButton onClick={() => openConfirmDialog(row.id)} color="secondary">
+        <IconButton
+          onClick={(e) => {
+            e.stopPropagation();
+            openConfirmDialog(sclass._id);
+          }}
+          color="secondary"
+        >
           <DeleteIcon color="error" />
         </IconButton>
         <BlueButton
           variant="contained"
-          onClick={() => navigate("/Admin/classes/class/" + row.id)}
+          onClick={(e) => {
+            e.stopPropagation();
+            navigate("/Admin/classes/class/" + sclass._id);
+          }}
         >
           View
         </BlueButton>
-        <ActionMenu actions={actions} />
       </ButtonContainer>
-    );
-  };
-
-  const ActionMenu = ({ actions }) => {
-    const [anchorEl, setAnchorEl] = useState(null);
-
-    const open = Boolean(anchorEl);
-
-    const handleClick = (event) => {
-      setAnchorEl(event.currentTarget);
-    };
-    const handleClose = () => {
-      setAnchorEl(null);
-    };
-    return (
-      <>
-        <Box
-          sx={{ display: "flex", alignItems: "center", textAlign: "center" }}
-        >
-          <Tooltip title="Add Students & Subjects">
-            <IconButton
-              onClick={handleClick}
-              size="small"
-              sx={{ ml: 2 }}
-              aria-controls={open ? "account-menu" : undefined}
-              aria-haspopup="true"
-              aria-expanded={open ? "true" : undefined}
-            >
-              <h5>Add</h5>
-              <SpeedDialIcon />
-            </IconButton>
-          </Tooltip>
-        </Box>
-        <Menu
-          anchorEl={anchorEl}
-          id="account-menu"
-          open={open}
-          onClose={handleClose}
-          onClick={handleClose}
-          PaperProps={{
-            elevation: 0,
-            sx: styles.styledPaper,
-          }}
-          transformOrigin={{ horizontal: "right", vertical: "top" }}
-          anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
-        >
-          {actions.map((action, index) => (
-            <MenuItem key={index} onClick={action.action}>
-              <ListItemIcon fontSize="small">{action.icon}</ListItemIcon>
-              {action.name}
-            </MenuItem>
-          ))}
-        </Menu>
-      </>
-    );
-  };
+    </HoverableDiv>
+  );
 
   return (
     <>
       {loading ? (
         <div>Loading...</div>
       ) : (
-        <>
-          {getresponse ? (
-            <Box
-              sx={{
-                display: "flex",
-                justifyContent: "flex-end",
-                marginTop: "16px",
-              }}
+        <Paper
+          elevation={3}
+          style={{ padding: "20px", backgroundColor: "#fff" }}
+        >
+          <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
+            <GreenButton
+              variant="contained"
+              onClick={() => navigate("/Admin/addclass")}
             >
-              <GreenButton
-                variant="contained"
-                onClick={() => navigate("/Admin/addclass")}
+              Add Class
+            </GreenButton>
+          </Box>
+          {filteredClasses.map((sclass) => (
+            <SclassRow key={sclass._id} sclass={sclass} />
+          ))}
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <Select
+              value={rowsPerPage}
+              onChange={handleRowsPerPageChange}
+              displayEmpty
+            >
+              <MuiMenuItem value={5}>5</MuiMenuItem>
+              <MuiMenuItem value={10}>10</MuiMenuItem>
+              <MuiMenuItem value={25}>25</MuiMenuItem>
+            </Select>
+            <span>
+              Page {page + 1} of {Math.ceil(sclassesList.length / rowsPerPage)}
+            </span>
+            <div>
+              <Button
+                onClick={() => handlePageChange("prev")}
+                disabled={page === 0}
               >
-                Add Class
-              </GreenButton>
-            </Box>
-          ) : (
-            <>
-              {sclassesList?.length > 0 && (
-                <TableTemplate
-                  buttonHaver={SclassButtonHaver}
-                  columns={sclassColumns}
-                  rows={sclassRows}
-                />
-              )}
-              <Box
-                sx={{
-                  position: "fixed",
-                  bottom: 16,
-                  right: 16,
-                  zIndex: 9999,
-                }}
+                Previous
+              </Button>
+              <Button
+                onClick={() => handlePageChange("next")}
+                disabled={
+                  page >= Math.ceil(sclassesList.length / rowsPerPage) - 1
+                }
               >
-                <BlueButton
-                  variant="contained"
-                  onClick={() => navigate("/Admin/addclass")}
-                  sx={{
-                    backgroundColor: "#1976d2 !important",
-                    position: "fixed",
-                    top: 600,
-                    right: 20,
-                    zIndex: 9999,
-                    "&:hover": {
-                      backgroundColor: "#3019d2 !important",
-                    },
-                  }}
-                >
-                  Add New Class
-                </BlueButton>
-              </Box>
-            </>
-          )}
-        </>
+                Next
+              </Button>
+            </div>
+          </div>
+        </Paper>
       )}
       <Popup
         message={message}
@@ -247,35 +201,23 @@ const ShowClasses = () => {
 
 export default ShowClasses;
 
-const styles = {
-  styledPaper: {
-    overflow: "visible",
-    filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
-    mt: 1.5,
-    "& .MuiAvatar-root": {
-      width: 32,
-      height: 32,
-      ml: -0.5,
-      mr: 1,
-    },
-    "&:before": {
-      content: '""',
-      display: "block",
-      position: "absolute",
-      top: 0,
-      right: 14,
-      width: 10,
-      height: 10,
-      bgcolor: "background.paper",
-      transform: "translateY(-50%) rotate(45deg)",
-      zIndex: 0,
-    },
-  },
-};
-
 const ButtonContainer = styled.div`
   display: flex;
   align-items: center;
-  justify-content: center;
   gap: 1rem;
+`;
+
+const HoverableDiv = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 10px;
+  margin: 5px 0;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+  transition: background-color 0.3s ease;
+
+  &:hover {
+    background-color: #f1f1f1;
+    cursor: pointer;
+  }
 `;
